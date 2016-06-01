7a0613500fbca3bb835c033e7466ee583032762b
Felix Gellner <flink@flinkblog.de>
2016-06-01 14:42:57 +0200

    Added first bachelor thesis presentation

---
+++ wege/typo3temp/Cache/Code/fluid_template/Standalone_template_source_08824175f2a1b4aab400593a9e871c2344dc2a9e.php
@@ -0,0 +1,31 @@
+<?php
+class FluidCache_Standalone_template_source_08824175f2a1b4aab400593a9e871c2344dc2a9e extends \TYPO3\CMS\Fluid\Core\Compiler\AbstractCompiledTemplate {
+
+public function getVariableContainer() {
+	// @todo
+	return new \TYPO3\CMS\Fluid\Core\ViewHelper\TemplateVariableContainer();
+}
+public function getLayoutName(\TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$currentVariableContainer = $renderingContext->getTemplateVariableContainer();
+
+return NULL;
+}
+public function hasLayout() {
+return FALSE;
+}
+
+/**
+ * Main Render function
+ */
+public function render(\TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+$currentVariableContainer = $renderingContext->getTemplateVariableContainer();
+
+
+
+return 'Schildkröte';
+}
+
+
+}
+#1464787219    797       
